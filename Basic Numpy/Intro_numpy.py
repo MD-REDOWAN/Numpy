87372995f2b35068f90f0995ccd0d3fcb663f9e4
MD-REDOWAN <hossain2305341310@diu.edu.bd>
2025-08-09 23:51:09 +0600

    Numpy Introduction

---
+++ Basic Numpy/Intro_numpy.py
@@ -0,0 +1,19 @@
+"""
+NumPy is a Python library.
+
+NumPy is used for working with arrays.
+
+NumPy is short for "Numerical Python".
+
+"""
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+
+# NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and manipulate them very efficiently.
