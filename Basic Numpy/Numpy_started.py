b46076d3be3c73b33b77871f68dbfd74a21764a2
MD-REDOWAN <hossain2305341310@diu.edu.bd>
2025-08-09 23:46:21 +0600

    Numpy Getting Started

---
+++ Basic Numpy/Numpy_started.py
@@ -0,0 +1,17 @@
+
+# import numpy as 
+
+import numpy as np
+
+arr = np.array([1, 3, 5, 7, 9])
+
+print(arr)
+
+
+print("------------------------------------------")
+
+# Checking Numpy Version
+
+import numpy as np
+
+print(np.__version__)
