5ce0688f29bb772aea8ceffe0fd34bfacb85e49c
MD-REDOWAN <hossain2305341310@diu.edu.bd>
2025-08-11 17:23:01 +0600

    Updated

--- Basic Numpy/2D Arrays.py
+++ Basic Numpy/2D Arrays.py
@@ -1,7 +1,19 @@
 
+
+# Creating 2 D arrays
+
+
 import numpy as np
 
 arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
 print(arr)
+
+
+# Output 
+
+"""
+[[ 1  2  3  4  5]
+ [ 6  7  8  9 10]]
+ """
 
